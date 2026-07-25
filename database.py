86cb41155df458134f68de7382d52e06355de590
faiyankhan26 <faiyankhan26@gmail.com>
2026-07-25 13:44:11 +0530

    Hide database credentials using environment variables

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql+psycopg://postgres:Aman2222@localhost:5432/notesapp"
+DATABASE_URL= "postgresql+psycopg://postgres:YOUR_REAL_PASSWORD@localhost:5432/notesapp"
 
 engine = create_engine(DATABASE_URL)
 
